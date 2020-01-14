1df84e327d051420ff0003c17ea107c5f41c09f2
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2020-01-14 10:50:37 +1000

    ggplot: time series of daily pool height

--- 01_Pool logger data_Bremer.R
+++ 01_Pool logger data_Bremer.R
@@ -5,4 +5,21 @@
 # read in logging data
 setwd("../../")
 
+pool.level<-read.csv("data/Bremer Stream/PoolHeight2.csv")
 
+library(lubridate)
+obs.date<-as.Date(pool.level$DateTime,format="%d/%m/%Y")
+pool.level$"Day"<-day(obs.date)
+
+library(dplyr)
+daily.pool.depth<-pool.level%>%
+  group_by(Site,Year,Month,Day)%>%
+  summarise(Depth=mean(Depth2))%>%
+  ungroup()%>%
+  mutate(Date=as.Date(paste(Year,Month,Day,sep="/"),format="%Y/%m/%d"))
+
+library(ggplot2)
+daily.pool.depth%>%
+  ggplot(aes(x=Date,y=Depth,colour=Site))+
+  geom_line()+
+  facet_wrap(~Site)
